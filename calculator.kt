779c29cc4ecb396f961dfbac523a5276aada9a22
Lais Lemos <laisaplemos10@gmail.com>
2024-03-17 18:09:30 -0300

    Function add added

--- calculator.kt
+++ calculator.kt
@@ -2,8 +2,15 @@ class Calculator(val name: String) {
   init {
     println("$name's Calculator 🧮")
   }
+
+   //function add
+  fun add(param1: Int, param2: Int): Int {
+    print("$param1 + $param2 = ")
+    return param1 + param2
+  }
+  
 }
 
 fun main() {
-    
+
 }
